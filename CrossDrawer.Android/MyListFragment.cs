81a18625b8348bd2165e15077cd65c00e2bf5d35
strookers <michael_k@mail.dk>
2016-10-31 20:24:13 +0100

    init commit

---
+++ CrossDrawer.Android/MyListFragment.cs
@@ -0,0 +1,20 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using CrossDrawer.Core;
+using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Support.V7.Fragging.Attributes;
+using MvvmCross.Droid.Support.V7.Fragging.Fragments;
+
+namespace CrossDrawer.Android
+{
+	[MvxFragmentAttribute(typeof(MainViewModel), Resource.Id.frameLayout)]
+	[Register("crossdrawer.android.MyListFragment")]
+	public class MyListFragment : MvxFragment<MyListViewModel>
+	{
+        public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			return inflater.Inflate(Resource.Layout.MyListView, container, false);
+		}
+	}
+}
---
+++ CrossDrawer.Core/ViewModels/MyListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using CrossDrawer.Core.Models;
+using MvvmCross.Core.ViewModels;
+
+namespace CrossDrawer.Core
+{
+	public class MyListViewModel : MvxViewModel
+	{
+
+        private List<CampingPlace> _campingPlaces;
+        public List<CampingPlace> CampingPlaces
+        {
+            get { return _campingPlaces; }
+            set { _campingPlaces = value; RaisePropertyChanged(() => CampingPlaces); }
+        }
+
+        public MyListViewModel()
+	    {
+	        CampingPlace c1 = new CampingPlace {Id = 1, Name = "Søborg Camping", City = "Søborg"};
+            CampingPlace c2 = new CampingPlace { Id = 2, Name = "Viborg Camping", City = "Viborg" };
+            CampingPlace c3 = new CampingPlace { Id = 3, Name = "Herning Camping", City = "Herning" };
+            CampingPlace c4 = new CampingPlace { Id = 4, Name = "Aalborg Camping", City = "Aalborg" };
+
+            _campingPlaces = new List<CampingPlace>();
+            _campingPlaces.Add(c1);
+            _campingPlaces.Add(c2);
+            _campingPlaces.Add(c3);
+            _campingPlaces.Add(c4);
+	    }
+    }
+}
