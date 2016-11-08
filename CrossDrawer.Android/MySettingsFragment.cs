fd75b827daf9bd43174dd4d1b388b255263d5a1e
strookers <michael_k@mail.dk>
2016-11-08 22:23:23 +0100

    google map sat op

--- CrossDrawer.Android/MySettingsFragment.cs
+++ CrossDrawer.Android/MySettingsFragment.cs
@@ -1,4 +1,7 @@
-﻿using Android.OS;
+﻿using System;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using CrossDrawer.Core;
@@ -7,13 +10,32 @@ using MvvmCross.Droid.Support.V7.Fragging.Fragments;
 
 namespace CrossDrawer.Android
 {
-	[MvxFragmentAttribute(typeof(MainViewModel), Resource.Id.frameLayout)]
-	[Register("crossdrawer.android.MySettingsFragment")]
-	public class MySettingsFragment : MvxFragment<MySettingsViewModel>
-	{
-		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-		{
-			return inflater.Inflate(Resource.Layout.MySettingsView, container, false);
-		}
-	}
+    [MvxFragmentAttribute(typeof(MainViewModel), Resource.Id.frameLayout)]
+    [Register("crossdrawer.android.MySettingsFragment")]
+    public class MySettingsFragment : MvxFragment<MySettingsViewModel>, IOnMapReadyCallback
+    {
+        MapView mapView;
+        GoogleMap map;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var v = inflater.Inflate(Resource.Layout.MySettingsView, container, false);
+            mapView = (MapView)v.FindViewById(Resource.Id.map);
+            mapView.OnCreate(savedInstanceState);
+            mapView.OnResume();
+            mapView.GetMapAsync(this);
+            map = mapView.Map;
+            MapsInitializer.Initialize(this.Activity);
+
+            CameraUpdate cameraUpdate = CameraUpdateFactory.NewLatLngZoom(new LatLng(57.0551, 9.88488), 10);
+            map.AnimateCamera(cameraUpdate);
+            return v;
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            this.map = googleMap;
+        }
+    }
+
 }
